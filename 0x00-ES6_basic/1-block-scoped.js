9d2fca69d5bb6135ff48d2768c87067e16ec03fe
edwin254hub <edwinmutunga42@gmail.com>
2023-11-27 12:37:08 -0800

    add

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars, no-shadow
+    /* eslint-disable */
     const task = true;
-    // eslint-disable-next-line no-unused-vars, no-shadow
     const task2 = false;
   }
 
